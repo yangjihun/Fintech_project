50a149c8493bb919ca5fbb714c9f3ec0d46a3fa1
Jihun <yjhn0410@gmail.com>
2024-03-30 19:34:57 +0900

    try input data

--- map/prtc.js
+++ map/prtc.js
@@ -1,14 +1,13 @@
-let fs = require('fs');
-let store = []; // 식당 정보
+const fs = require('fs');   // file system 모듈 가져오기
+let a = []
+function c(a){
+  fs.readFile('test.txt', 'utf8', function(err, data){
+  // fs모듈의 readFile함수를 사용해 읽을 파일을 첫번째 인자로 가져오고 콜백함수의 두번째 인자(data)로 파일내용을 받음
+    a.push(data);
+    return a;
+  });
+};
 
-fs.readFile("착한가격업소.csv",'utf-8',function(err,data){
-    let n = data.split('\n');
-    let c = '';
-    for(let i=0;i<n.length;i++){
-      c = n[i].split(',');
-      store.push({name:c[3], locate:c[4]});
-    }
-    console.log(store);
-});
+let k = c(a);
 
-console.log(store);
+console.log(k);
